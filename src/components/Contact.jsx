e9060c565af15be8e64a0dc42c3943bf567faa92
Maarten De Weerdt <maarten.deweerdt@yellowjersey.be>
2019-09-02 19:47:59 +0200

    added contact options & socials

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,11 @@
 import React from "react"
 import { withTheme } from "emotion-theming"
+import {
+  FaLinkedin,
+  FaGithubSquare,
+  FaFacebookSquare,
+  FaInstagram,
+} from "react-icons/fa"
 import css from "@emotion/css"
 import Wave from "react-wavify"
 import { Content, Inner, Divider } from "../elements"
@@ -64,6 +70,83 @@ const Contact = ({ offset, contact, theme }) => (
     <Content speed={0.4} offset={offset}>
       <Inner css={css({ color: theme.colors.white })}>
         <h1 css={css({ color: theme.colors.white })}>Get in touch</h1>
+        <h4>
+          Email:{" "}
+          <a
+            href="mailto:maarten.dw@hotmail.be"
+            css={css({ color: theme.colors.white })}
+          >
+            maarten.dw@hotmail.be
+          </a>
+        </h4>
+        <h4>
+          Phone:{" "}
+          <a href="tel:0491224508" css={css({ color: theme.colors.white })}>
+            04 912 245 08
+          </a>
+        </h4>
+        <h3 css={css({ color: theme.colors.white })}>
+          or find me on the following platforms:
+        </h3>
+        <div>
+          <a
+            href="https://www.linkedin.com/in/maartendeweerdt"
+            target="_blank"
+            rel="noopener noreferrer"
+            css={css({
+              display: "inline-block",
+              color: theme.colors.white,
+              padding: "0.5rem",
+            })}
+          >
+            <h1>
+              <FaLinkedin />
+            </h1>
+          </a>
+          <a
+            href="https://github.com/MaartenDeWeerdt"
+            target="_blank"
+            rel="noopener noreferrer"
+            css={css({
+              display: "inline-block",
+              color: theme.colors.white,
+              padding: "0.5rem",
+            })}
+          >
+            <h1>
+              <FaGithubSquare />
+            </h1>
+          </a>
+          <a
+            href="https://www.facebook.com/maarten.deweerdt.1"
+            target="_blank"
+            rel="noopener noreferrer"
+            css={css({
+              display: "inline-block",
+              color: theme.colors.white,
+              padding: "0.5rem",
+            })}
+          >
+            <h1>
+              <FaFacebookSquare />
+            </h1>
+          </a>
+          <a
+            href="https://www.instagram.com/maartendeweerdt"
+            target="_blank"
+            rel="noopener noreferrer"
+            css={css({
+              display: "inline-block",
+              color: theme.colors.white,
+              padding: "0.5rem",
+            })}
+          >
+            <h1>
+              <FaInstagram />
+            </h1>
+          </a>
+        </div>
+
         <Footer />
       </Inner>
     </Content>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from "react"
 import { withTheme } from "emotion-theming"
 import css from "@emotion/css"
 
-const Footer = ({ theme }) => (
-  <div>
+const Footer = () => (
+  <div css={css({ position: "absolute", bottom: "1rem" })}>
     <p css={css({ textAlign: "center" })}>
       Copyright &copy; {new Date().getFullYear()}. All rights reserved.
     </p>
